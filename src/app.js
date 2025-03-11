417d1ef8ac7304813ecc16ab66313b165878b6db
dendenmuniz <dendenmuniz@gmail.com>
2025-03-11 17:18:25 +0100

    refactor: improve login and error treatment; separate config and init

--- src/app.js
+++ src/app.js
@@ -2,18 +2,19 @@ const express = require("express");
 const dotenv = require("dotenv");
 const trailerRouter = require("./routes/trailer");
 const globalErrorHandler = require("./middleware/globalErrorHandler");
-
-const app = express();
+const morgan = require("morgan");
 
 dotenv.config({ path: "./config/config.env" });
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.use(express.json());
+app.use(morgan("dev")); 
+
 app.use("/api", trailerRouter);
 
-// global Error Handler to all requests
+
 app.use(globalErrorHandler);
 
 const server = app.listen(PORT, () => {
