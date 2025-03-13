9c60b478707d32a6df96476f5f0bbb71ba119765
dendenmuniz <dendenmuniz@gmail.com>
2025-03-13 11:31:47 +0100

    feat:add security enhancements with validation, rate limiting fix .env

--- src/app.js
+++ src/app.js
@@ -1,19 +1,30 @@
 const express = require("express");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 const dotenv = require("dotenv");
 const trailerRouter = require("./routes/trailer");
 const globalErrorHandler = require("./middleware/globalErrorHandler");
 const morgan = require("morgan");
 
-dotenv.config({ path: "./config/config.env" });
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(morgan("dev")); 
+app.use(morgan("dev"));
+app.use(helmet());
 
+//  Rate Limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Max de 100 req for IP
+  message: "Too many requests from this IP, please try again later.",
+  headers: true,
+});
+
+app.use("/api", limiter);
 app.use("/api", trailerRouter);
-
 
 app.use(globalErrorHandler);
 
